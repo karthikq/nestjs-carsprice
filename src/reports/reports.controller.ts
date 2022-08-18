83e18bcf794758ff84b5b880152228769099a9f5
karthikq <kk968346@gmail.com>
2022-08-18 11:27:36 +0530

    test

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Injectable } from '@nestjs/common';
+import { Body, Controller, Injectable, Post } from '@nestjs/common';
+import { createReportDto } from './dto/create-report.dto';
+import { ReportService } from './reports.service';
 
 @Controller('reports')
-export class ReportsContoller {}
+export class ReportsContoller {
+  constructor(private reportsSerive: ReportService) {}
+
+  @Post()
+  createReport(@Body() body: createReportDto) {
+    this.reportsSerive.createReport(body);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { createReportDto } from './dto/create-report.dto';
+import { Report } from './reports.entity';
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+  createReport(body: createReportDto) {
+    console.log(body);
+  }
+}
